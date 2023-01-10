3c759092e80ed3d5c1b938664a6b974d28bcc36c
Yasir Younas <yasiryounas@yasirs-air.home>
2023-01-10 14:43:44 +0000

    finshort words

---
+++ findshortWord.js
@@ -0,0 +1,18 @@
+//Simple, given a string of words, return the length of the shortest word(s).
+
+//String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+  
+    let word = s.split(' ');
+    
+    let shortLength = Infinity
+    
+    for (let i = 0; i < word.length; i++) {
+       if(word[i].length < shortLength){
+         shortLength = word[i].length
+       }
+    }
+    return shortLength
+  }
+
