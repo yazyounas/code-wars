fc76fbc57e44f21aa116f375d290cabbf97155e1
Yasir Younas <yasiryounas@yasirs-air.home>
2023-01-10 20:26:17 +0000

    filtered and return string and integer

---
+++ filterstingint.js
@@ -0,0 +1,16 @@
+
+//In this kata you will create a function that takes a 
+//list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+    let filteredList = []
+    for (let i = 0; i < l.length; i++) {
+      if (typeof l[i] === 'number'){
+        filteredList.push(l[i])
+      }
+    }
+    return filteredList
+    
+     }
